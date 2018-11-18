e350d082f7c50e69e80bf87f2a02b42b84fc056a
Lukas Burk <lukas@quantenbrot.de>
2018-11-18 01:11:00 +0100

    Recoding

--- 04_mehr_daten.R
+++ 04_mehr_daten.R
@@ -26,3 +26,51 @@ sjt.xtab(qm$gender, qm$rauchen, show.row.prc = TRUE)
 sjt.xtab(qm$rauchen, qm$cannabis,
          show.row.prc = TRUE, show.col.prc = TRUE, show.cell.prc = TRUE,
          show.legend = TRUE)
+
+# Außreißer und Rekodieren ----
+
+# Wir betrachten mal die Variable "Freundeskreis"
+range(qm$freundeskreis) # Spannweite: Kleinster und größter Wert
+quantile(qm$freundeskreis)
+
+# Das ist viel Spannweite. Mal als Histogramm:
+ggplot(data = qm, aes(x = freundeskreis)) +
+  geom_histogram(binwidth = 10)
+
+# Uff. Vielleicht wollen wir alle Leute mit mehr als 100 Freunden rauskicken.
+
+# Nehme qm, filtere, so dass nur Leute mit "< 100" Freunden bleiben, speichere in qm
+qm <- qm %>%
+  filter(freundeskreis < 100)
+
+# Nochmal gucken:
+ggplot(data = qm, aes(x = freundeskreis)) +
+  geom_histogram(binwidth = 10)
+
+# Viel handlicher!
+
+# Gruppieren (=Rekodieren) ----
+library(sjmisc)
+# Rekodieren via rec(), siehe auch Hilfe und Beispiele unter
+?rec
+
+qm$freundeskreis_klassiert <- rec(qm$freundeskreis, rec = "0:10=1; 11:20=2; 21:30=3; 31:max=4")
+
+# Jetzt machen wir ein ordinales Merkmal draus, mit Labels
+qm$freundeskreis_klassiert <- factor(
+  qm$freundeskreis_klassiert,
+  labels = c("Wenige", "Mittel", "Viele", "Sehr viele"),
+  ordered = TRUE
+)
+
+# Angucken
+qm$freundeskreis_klassiert
+
+# Viel handlicher!
+ggplot(data = qm, aes(x = freundeskreis_klassiert)) +
+  geom_bar()
+
+# Vielleicht jetzt mit bunt?
+ggplot(data = qm, aes(x = freundeskreis_klassiert)) +
+  geom_bar(fill = "darkred", color = "black") +
+  theme_tadaa()
